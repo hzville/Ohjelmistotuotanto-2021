726d466a89e3a1f7e51804f0e05c4fbe987dad8d
Ville Halonen <71012693+hzville@users.noreply.github.com>
2021-11-27 18:41:42 +0200

    ex. 12

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -67,9 +67,17 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 2)
     
-    def test_kaksi_samaa_tuotetta_ostos_lukumaara(self):
+    def test_kaksi_samaa_tuotetta_ostoslista_lukumaara(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+
+    def test_kaksi_samaa_tuotetta_ostos_nimi_ja_lukumaara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), 'Maito')
+        self.assertEqual(ostos.lukumaara(), 2)
